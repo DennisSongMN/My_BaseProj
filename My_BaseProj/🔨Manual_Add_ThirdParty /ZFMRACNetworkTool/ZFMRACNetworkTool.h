97f144128dbf14efb32e09107a45f7b852b36783
Kite <Kite>
2019-06-10 17:31:28 +0800

    no message

--- My_BaseProj/🔨Manual_Add_ThirdParty /ZFMRACNetworkTool/ZFMRACNetworkTool.h
+++ My_BaseProj/🔨Manual_Add_ThirdParty /ZFMRACNetworkTool/ZFMRACNetworkTool.h
@@ -15,3 +15,35 @@
 #import "FMARCNetwork.h"
 
 #endif /* ZFMRACNetworkTool_h */
+
+/* 使用示例:
+
+- (void)test{
+ 
+    /// 1. 配置参数
+    NSMutableDictionary *easyDict = [NSMutableDictionary dictionary];
+    /// 2. 配置参数模型 #define MH_GET_LIVE_ROOM_LIST  @"Room/GetHotLive_v2"
+    FMHttpRequest *req = [FMHttpRequest urlParametersWithMethod:HTTTP_METHOD_POST path:@"Room/GetHotLive_v2" parameters:easyDict];
+    
+    
+    _reqSignal = [[FMARCNetwork sharedInstance] requestNetworkData:req];
+    
+    [_reqSignal subscribeNext:^(FMHttpResonse *response) {
+        
+        if (response.isSuccess) {
+            
+            NSLog(@"%p",response.reqResult);
+            
+            NSLog(@"--%@",response.reqResult);
+            
+            NSData *jsonData = [NSJSONSerialization dataWithJSONObject:response.reqResult options:NSJSONWritingPrettyPrinted error:nil];
+            
+            NSString * str = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+            
+            self.textV.text = str;
+        }
+        
+    }];
+}
+
+*/
