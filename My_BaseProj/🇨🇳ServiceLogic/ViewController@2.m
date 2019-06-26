531c44ec3ecf14dfe34b908ed474f4879f18c31c
geng liu <lg295060456@gmail.com>
2019-06-26 11:02:24 +0800

    no message

--- My_BaseProj/🇨🇳ServiceLogic/ViewController@2.m
+++ My_BaseProj/🇨🇳ServiceLogic/ViewController@2.m
@@ -10,6 +10,15 @@
 #import "HistoryDataListTBV.h"
 #import "HistoryDataListTBVCell.h"
 
+///视频框架
+//#import <libavformat/avformat.h>
+//#import <libavcodec/avcodec.h>
+//#import <libswscale/swscale.h>
+//#import <libavutil/avutil.h>
+//#import <libswresample/swresample.h>
+//#import <libavdevice/avdevice.h>
+//#import <libavfilter/avfilter.h>
+
 @interface ViewController_2 (){
 }
 
@@ -70,6 +79,23 @@
     BaseNavigationVC *nav = (BaseNavigationVC *)self.navigationController;
 
     [nav setupEditBtn:self];
+    
+    //视频相关
+//    av_register_all();
+//    avcodec_register_all();
+//    avformat_network_init();
+//    AVFormatContext *avFormatContext = avformat_alloc_context();
+//
+//    NSString *url = @"rtmp://v1.one-tv.com:1935/live/mpegts.stream";
+//    if (avformat_open_input(&avFormatContext, [url UTF8String], NULL, NULL) != 0) {
+//        av_log(NULL, AV_LOG_ERROR, "Couldn't open file");
+//    }
+//
+//    if (avformat_find_stream_info(avFormatContext, NULL) < 0) {
+//        av_log(NULL, AV_LOG_ERROR, "Couldn't find stream information");
+//    } else {
+//        av_dump_format(avFormatContext, 0, [url cStringUsingEncoding:NSUTF8StringEncoding], NO);
+//    }
 }
 
 -(void)btnClickEvent:(UIButton *)sender{
