0c3c27cec205045b69675b2df2b24cce3112be9e
geng liu <lg295060456@gmail.com>
2019-06-29 16:43:10 +0800

    no message

--- My_BaseProj/🇨🇳ServiceLogic/ArcTBVCell.h
+++ My_BaseProj/🇨🇳ServiceLogic/ArcTBVCell.h
@@ -16,6 +16,8 @@ NS_ASSUME_NONNULL_BEGIN
 
 +(CGFloat)cellHeight:(id)model;
 
+-(void)richElementsInCellWithModel:(id)model;
+
 @end
 
 NS_ASSUME_NONNULL_END
--- My_BaseProj/🇨🇳ServiceLogic/ArcTBVCell.m
+++ My_BaseProj/🇨🇳ServiceLogic/ArcTBVCell.m
@@ -35,6 +35,10 @@
     return 30;
 }
 
+-(void)richElementsInCellWithModel:(id)model{
+    
+}
+
 -(instancetype)initWithStyle:(UITableViewCellStyle)style
              reuseIdentifier:(NSString *)reuseIdentifier{
     
@@ -46,21 +50,6 @@
     }
     
     return self;
-}
-
-
-
-
-
-- (void)awakeFromNib {
-    [super awakeFromNib];
-    // Initialization code
-}
-
-- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
-    [super setSelected:selected animated:animated];
-
-    // Configure the view for the selected state
 }
 
 @end
