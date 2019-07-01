57fc1d03ebd7ef891e53cd2b5b0b33f50f5e9c72
geng liu <lg295060456@gmail.com>
2019-07-01 12:13:44 +0800

    no message

--- My_BaseProj/🔨Manual_Add_ThirdParty /🌍ZFMRACNetworkTool/Constant/FMHttpConstant.h
+++ My_BaseProj/🔨Manual_Add_ThirdParty /🌍ZFMRACNetworkTool/Constant/FMHttpConstant.h
@@ -14,9 +14,39 @@
 #define FMHttpConstant_h
 
 /*******URL******/
-#define BaseUrl @"https://live.9158.com/"
+#define BaseUrl @"http://googshoping.colors8.com.cn/"
 /********ImgBaseURL*****/
 #define ImgBaseURL @""
+//api
+//获取文章
+#define GettingArticle @"api/index/getArticle" //POST 参数：category=1(1赛事相关4战队动态5娱乐周围)   page=1
+//获取轮播
+#define GettingCarouselPic @"api/index/getArticle"
+//文章详情
+#define GettingArticleDetail @"portal/article/index/id/{id}"//?
+//用户登录信息
+#define GettingUserLoginInfo @"api/index/center?uid=1159"
+//视频
+#define GettingVedio @"portal/list/index/id/8"
+//开黑
+#define GettingGangUp @"portal/list/index/id/7"
+//二维码
+#define GettingQRCode @"api/index/erweima"
+//官网连接
+#define GettingOfficialWebsite @"portal/article/index/id/30"
+//个人资料
+#define Personaldata @"portal/article/index/id/32"
+//我的评论
+#define MyComment @"portal/article/index/id/33"
+//关于我们
+#define AboutUs @"portal/article/index/id/34"
+//注册
+#define SignIn @"user/register/doRegister" //POST 参数：password  username
+//登录
+#define Login @"user/login/dologin" //参数：password  username
+
+#pragma mark - api拼接
+#define API(DomainName,api) [NSString stringWithFormat:@"%@%@",BaseUrl,api]
 
 /********如果需要存储，相应的的 key 宏定义********/
 /// 服务器相关
@@ -28,7 +58,6 @@
 #define HTTPServiceKey  @"privatekey"
 /// 私钥Value
 #define HTTPServiceKeyValue @"/** 你的私钥 **/"
-
 
 /// 状态码key
 #define HTTPServiceResponseCodeKey @"code"
