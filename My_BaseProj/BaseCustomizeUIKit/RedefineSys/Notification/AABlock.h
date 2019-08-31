1bdd2defc29036b5c2ea136cd0c2f82cf4fe7b49
Whiskey_On_The_Rocks <lg295060456@gmail.com>
2019-08-31 11:12:13 +0800

    no message

--- My_BaseProj/BaseCustomizeUIKit/RedefineSys/Notification/AABlock.h
+++ My_BaseProj/BaseCustomizeUIKit/RedefineSys/Notification/AABlock.h
@@ -1,40 +1,12 @@
-//
-//  AABlock.h
 
-//
-//  Created by Aalto on 2018/10/27.
-//  Copyright © 2018年 NickJackson. All rights reserved.
-//
-
-
-typedef void (^ActionBlock)(id data);
-typedef void (^DataBlock)(id data);
-
-typedef void (^TwoDataBlock)(id data,id data2);
-
-typedef void (^DataTypeBlock)(NSInteger types,id data);
-
-typedef void (^TableViewDataBlock)(id data,id data2,UIView* view,UITableView* tableView,NSMutableArray *dataSource,NSIndexPath* indexPath);
-typedef void (^ThreeDataBlock)(id data,id data2,id data3);
-typedef void (^EightDataBlock)(id data,id data2,id data3,id data4,id data5,id data6,id data7,id data8);
-typedef void (^DataActionBlock)(id data, BOOL action);
-
-typedef void (^TypeBlock)(NSInteger type);
-typedef void (^IntegerBlock)(NSInteger type);
-
-
-typedef void (^NoResultBlock)(void);
-typedef void (^KeyboardBlock)(id view);
-
-typedef void (^CheckBlock)(BOOL finish, NSInteger errorType);
-
-typedef void (^RequestResultBlock)(id object, NSError *error);
-
-
-typedef void (^FilmDetailBlock)(id data,BOOL failFlag);
-
-typedef void (^GoodsDeleteBlock)(NSInteger section);
-
-typedef void (^CountDownBlock)(NSTimer *timer, NSTimeInterval remindTime, NSString *tip);
-
-
+typedef void(^NoResultBlock)(void);
+typedef void(^DataBlock)(id data);
+typedef void(^TwoDataBlock)(id data,id data2);
+typedef void(^ThreeDataBlock)(id data,id data2,id data3);
+typedef void(^FourDataBlock)(id data,id data2,id data3,id data4);
+typedef void(^FiveDataBlock)(id data,id data2,id data3,id data4,id data5);
+typedef void(^SixDataBlock)(id data,id data2,id data3,id data4,id data5,id data6);
+typedef void(^SevenDataBlock)(id data,id data2,id data3,id data4,id data5,id data7);
+typedef void(^EightDataBlock)(id data,id data2,id data3,id data4,id data5,id data7,id data8);
+typedef void(^NineDataBlock)(id data,id data2,id data3,id data4,id data5,id data7,id data8,id data9);
+typedef void(^TenDataBlock)(id data,id data2,id data3,id data4,id data5,id data7,id data8,id data9,id data10);
