c5bcb8a5affb382c345ec6373cee59c5d284fb17
Kite <>
2019-10-26 14:42:55 +0800

    no message

--- My_BaseProj/🔨Manual_Add_ThirdParty /ToastView/YKToastView.h
+++ My_BaseProj/🔨Manual_Add_ThirdParty /ToastView/YKToastView.h
@@ -13,16 +13,17 @@ NS_ASSUME_NONNULL_BEGIN
 @interface YKToastView : UIView
 
 /**
-	Toast样式提示自定义内容
+    Toast样式提示自定义内容
  
-	@param text 自定义文本
-	@param view 要显示的父视图
+    @param text 自定义文本
+    @param view 要显示的父视图
  */
-+ (void)showToastText:(NSString *)text inView:(UIView *)view;
++ (void)showToastText:(NSString *)text
+               inView:(UIView *)view;
 /**
-	Toast样式在Window上提示自定义内容
+    Toast样式在Window上提示自定义内容
  
-	@param text 自定义文本
+    @param text 自定义文本
  */
 + (void)showToastText:(NSString *)text;
 
@@ -31,7 +32,8 @@ NS_ASSUME_NONNULL_BEGIN
  
  @param text 自定义文本 second 停留多少秒（单位是秒）
  */
-+ (void)showToastText:(NSString *)text withDisplayTime:(NSTimeInterval)iSecond;
++ (void)showToastText:(NSString *)text
+      withDisplayTime:(NSTimeInterval)iSecond;
 
 /**
  Toast样式在Window上提示自定义内容，位置随机
--- My_BaseProj/🔨Manual_Add_ThirdParty /ToastView/YKToastView.m
+++ My_BaseProj/🔨Manual_Add_ThirdParty /ToastView/YKToastView.m
@@ -8,27 +8,22 @@
 
 #import "YKToastView.h"
 
-//渐入动画时间
-#define kYKToastViewFadeinDuration 0.2
-//停留显示时间
-#define kYKToastViewDisplayDuration 2.5
-//渐出动画时间
-#define kYKToastViewFadeoutDuration 0.3
-//提示文本字体
-#define kYKToastViewTextFont [UIFont systemFontOfSize:15]
-
+#define kYKToastViewFadeinDuration 0.2//渐入动画时间
+#define kYKToastViewDisplayDuration 2.5//停留显示时间
+#define kYKToastViewFadeoutDuration 0.3//渐出动画时间
+#define kYKToastViewTextFont [UIFont systemFontOfSize:15]//提示文本字体
 #define SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(v)  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedAscending)
 
 @class YKToastMaskWindow;
 
 @interface YKToastView(){
-    
     YKToastMaskWindow *maskWindow_;
 }
 
-@property (nonatomic, copy) NSString *tipText;
-
-@property (nonatomic, strong) UILabel *tipLabelView;
+@property(nonatomic,copy)NSString *tipText;
+@property(nonatomic,strong)UILabel *tipLabelView;
+@property(nonatomic,strong)CABasicAnimation *fadeInAnimation;
+@property(nonatomic,strong)CABasicAnimation *fadeOutAnimation;
 
 - (void)forceHide;
 
@@ -44,273 +39,205 @@
 
 - (void)touchesBegan:(NSSet *)touches
            withEvent:(UIEvent *)event{
-    
-    [super touchesBegan:touches withEvent:event];
-    
+    [super touchesBegan:touches
+              withEvent:event];
     [(YKToastView *)self.maskView forceHide];
 }
 
 @end
 
 @implementation YKToastView
-
 #pragma mark - --------------------退出清空--------------------
-
 - (void)dealloc{
-    
     NSLog(@"Running self.class = %@;NSStringFromSelector(_cmd) = '%@';__FUNCTION__ = %s", self.class, NSStringFromSelector(_cmd),__FUNCTION__);
 }
-
 #pragma mark - --------------------初始化--------------------
-
 - (id)initWithFrame:(CGRect)frame{
-
     if (self = [super initWithFrame:frame]) {
-   
         [self initBaseView];
-    }
-    
-    return self;
+    }return self;
 }
 
 - (id)initWithCoder:(NSCoder *)aDecoder{
-
     if (self = [super initWithCoder:aDecoder]) {
-        
         [self initBaseView];
-    }
-    
-    return self;
+    }return self;
 }
 
 - (void)awakeFromNib{
-    
     [super awakeFromNib];
-    
     [self initBaseView];
 }
 
 - (void)setFrame:(CGRect)frame{
-    
     [super setFrame:frame];
-    
     if (_tipLabelView) {
-        
         int edge = 10;
-        
-        [_tipLabelView setFrame:UIEdgeInsetsInsetRect(self.bounds, UIEdgeInsetsMake(edge, edge, edge, edge))];
+        [_tipLabelView setFrame:UIEdgeInsetsInsetRect(self.bounds,
+                                                      UIEdgeInsetsMake(edge,
+                                                                       edge,
+                                                                       edge,
+                                                                       edge))];
     }
 }
 
 - (void)initBaseView{
-    
     [self setBackgroundColor:[[UIColor blackColor] colorWithAlphaComponent:0.7]];
-    
-    if (!_tipLabelView) {
-        
-        int edge = 10;
-        
-        _tipLabelView = [[UILabel alloc] initWithFrame:UIEdgeInsetsInsetRect(self.bounds, UIEdgeInsetsMake(edge, edge, edge, edge))];
-        
-        [_tipLabelView setBackgroundColor:[UIColor clearColor]];
-        
-        [_tipLabelView setTextAlignment:NSTextAlignmentCenter];
-        
-        [_tipLabelView setTextColor:[UIColor whiteColor]];
-        
-        [_tipLabelView setFont:kYKToastViewTextFont];
-        
-        _tipLabelView.numberOfLines = INT_MAX;
-    }
-    
-    [self addSubview:_tipLabelView];
-    
+    self.tipLabelView.alpha = 1;
     [self setClipsToBounds:YES];
-    
     [self.layer setCornerRadius:5];
+}
+#pragma mark —— lazyLoad
+-(UILabel *)tipLabelView{
+    if (!_tipLabelView) {
+        int edge = 10;
+        _tipLabelView = [[UILabel alloc] initWithFrame:UIEdgeInsetsInsetRect(self.bounds,
+                                                                             UIEdgeInsetsMake(edge,
+                                                                                              edge,
+                                                                                              edge,
+                                                                                              edge))];
+        [_tipLabelView setBackgroundColor:[UIColor clearColor]];
+        [_tipLabelView setTextAlignment:NSTextAlignmentCenter];
+        [_tipLabelView setTextColor:[UIColor whiteColor]];
+        [_tipLabelView setFont:kYKToastViewTextFont];
+        _tipLabelView.numberOfLines = INT_MAX;
+        [self addSubview:_tipLabelView];
+    }return _tipLabelView;
 }
 
 - (void)willMoveToWindow:(UIWindow *)newWindow{
-    
     [super willMoveToWindow:newWindow];
-    
     if (newWindow) {
-        
         if (_tipLabelView) {
-            
             [_tipLabelView setText:self.tipText];
         }
     }
 }
-
-#pragma mark - --------------------功能函数--------------------
-
+#pragma mark - --------------------功能函数-------------------
 - (void)showInView:(UIView *)view{
-    
     if (!maskWindow_) {
-        
         maskWindow_ = [[YKToastMaskWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-        
         maskWindow_.windowLevel = UIWindowLevelStatusBar + 1;
-        
         [maskWindow_ setBackgroundColor:[UIColor clearColor]];
     }
     maskWindow_.maskView = self;
-    
     [maskWindow_ makeKeyAndVisible];
-    
     view = maskWindow_;
-    
-    [self setCenter:CGPointMake(view.bounds.size.width/2.0, view.bounds.size.height/2.0)];
-    
+    [self setCenter:CGPointMake(view.bounds.size.width/2.0,
+                                view.bounds.size.height/2.0)];
     self.layer.opacity = 0.0;
-    
     [view addSubview:self];
-    
     [self fadeInAnimationAfterDelay:kYKToastViewDisplayDuration];
 }
 
-- (void)showInView:(UIView *)view withDisplayTime:(NSTimeInterval)iSecond{
-    
+- (void)showInView:(UIView *)view
+   withDisplayTime:(NSTimeInterval)iSecond{
     if (!maskWindow_) {
-        
         maskWindow_ = [[YKToastMaskWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-        
         maskWindow_.windowLevel = UIWindowLevelStatusBar + 1;
-        
         [maskWindow_ setBackgroundColor:[UIColor clearColor]];
     }
-    
     maskWindow_.maskView = self;
-    
     [maskWindow_ makeKeyAndVisible];
-    
     view = maskWindow_;
-    
-    [self setCenter:CGPointMake(view.bounds.size.width/2.0, view.bounds.size.height/2.0)];
-    
+    [self setCenter:CGPointMake(view.bounds.size.width/2.0,
+                                view.bounds.size.height/2.0)];
     self.layer.opacity = 0.0;
-    
     [view addSubview:self];
-    
     [self fadeInAnimationAfterDelay:iSecond];
 }
 
 - (void)fadeInAnimationAfterDelay:(NSTimeInterval)delay{
-    
     [CATransaction begin];
-    
+    @weakify(self)
     [CATransaction setCompletionBlock:^{
-        
-        [self performSelector:@selector(fadeOutAnimation)
+        @strongify(self)
+        [self performSelector:@selector(FadeOut_Animation)
                    withObject:nil
                    afterDelay:kYKToastViewDisplayDuration];
     }];
-    
-    CABasicAnimation *fadeInAnimation = [CABasicAnimation animationWithKeyPath:@"opacity"];
-    
-    [fadeInAnimation setDuration:kYKToastViewFadeinDuration];
-    
-    [fadeInAnimation setFromValue:[NSNumber numberWithFloat:0.0]];
-    
-    [fadeInAnimation setToValue:[NSNumber numberWithFloat:1.0]];
-    
-    [fadeInAnimation setRemovedOnCompletion:NO];
-    
-    [fadeInAnimation setFillMode:kCAFillModeForwards];
-    
-    [fadeInAnimation setTimingFunction:[CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionLinear]];
-    
-    [self.layer addAnimation:fadeInAnimation
+    [self.layer addAnimation:self.fadeInAnimation
                       forKey:@"fadeIn"];
-    
-    [CATransaction commit];
 }
 
-- (void)fadeOutAnimation{
-    
+- (void)FadeOut_Animation{
     maskWindow_.maskView = nil;
-    
     [CATransaction begin];
-    
+    @weakify(self)
     [CATransaction setCompletionBlock:^{
-        
+        @strongify(self)
         [self forceHide];
     }];
-    
-    CABasicAnimation *fadeOutAnimation = [CABasicAnimation animationWithKeyPath:@"opacity"];
-    
-    [fadeOutAnimation setDuration:kYKToastViewFadeoutDuration];
-    
-    [fadeOutAnimation setFromValue:[NSNumber numberWithFloat:1.0]];
-    
-    [fadeOutAnimation setToValue:[NSNumber numberWithFloat:0.0]];
-    
-    [fadeOutAnimation setRemovedOnCompletion:NO];
-    
-    [fadeOutAnimation setFillMode:kCAFillModeForwards];
-    
-    [fadeOutAnimation setTimingFunction:[CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionLinear]];
-    
-    [self.layer addAnimation:fadeOutAnimation
+    [self.layer addAnimation:self.fadeOutAnimation
                       forKey:@"fadeOut"];
-    
-    [CATransaction commit];
+}
+#pragma mark —— lazyLoad
+-(CABasicAnimation *)fadeInAnimation{
+    if (!_fadeInAnimation) {
+        _fadeInAnimation = [CABasicAnimation animationWithKeyPath:@"opacity"];
+        [_fadeInAnimation setDuration:kYKToastViewFadeinDuration];
+        [_fadeInAnimation setFromValue:[NSNumber numberWithFloat:0.0]];
+        [_fadeInAnimation setToValue:[NSNumber numberWithFloat:1.0]];
+        [_fadeInAnimation setRemovedOnCompletion:NO];
+        [_fadeInAnimation setFillMode:kCAFillModeForwards];
+        [_fadeInAnimation setTimingFunction:[CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionLinear]];
+        [self.layer addAnimation:_fadeInAnimation
+                          forKey:@"fadeIn"];
+        [CATransaction commit];
+    }return _fadeInAnimation;
+}
+
+-(CABasicAnimation *)fadeOutAnimation{
+    if (!_fadeOutAnimation) {
+        _fadeOutAnimation = [CABasicAnimation animationWithKeyPath:@"opacity"];
+        [_fadeOutAnimation setDuration:kYKToastViewFadeoutDuration];
+        [_fadeOutAnimation setFromValue:[NSNumber numberWithFloat:1.0]];
+        [_fadeOutAnimation setToValue:[NSNumber numberWithFloat:0.0]];
+        [_fadeOutAnimation setRemovedOnCompletion:NO];
+        [_fadeOutAnimation setFillMode:kCAFillModeForwards];
+        [_fadeOutAnimation setTimingFunction:[CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionLinear]];
+        [CATransaction commit];
+    }return _fadeOutAnimation;
 }
 
 #pragma mark - --------------------接口API--------------------
 #pragma mark 强制消失
-- (void)forceHide
-{
+- (void)forceHide{
     maskWindow_.maskView = nil;
-    
     [NSObject cancelPreviousPerformRequestsWithTarget:self
-                                             selector:@selector(fadeOutAnimation)
+                                             selector:@selector(FadeOut_Animation)
                                                object:nil];
-    
     [self removeFromSuperview];
-    
     [maskWindow_ resignKeyWindow];
-    
     [[[[UIApplication sharedApplication] delegate] window] makeKeyWindow];
 }
 
 #pragma mark Toast样式提示自定义内容
 + (void)showToastText:(NSString *)text
                inView:(UIView *)view{
-    
     if (![NSString isEmpty:text]){
-        
-        YKToastView *toastTipView = [[YKToastView alloc] initWithFrame:CGRectMake(0, 0, 250, 44)];
-        
+        YKToastView *toastTipView = [[YKToastView alloc] initWithFrame:CGRectMake(0,
+                                                                                  0,
+                                                                                  250,
+                                                                                  44)];
         CGSize textSize = CGSizeZero;
-        
         if (SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"7.0")){
-            
-            CGRect frame = [text boundingRectWithSize:CGSizeMake(250-30, 320)
+            CGRect frame = [text boundingRectWithSize:CGSizeMake(250 - 30, 320)
                                               options:(NSStringDrawingUsesLineFragmentOrigin | NSStringDrawingUsesFontLeading)
                                            attributes:@{NSFontAttributeName:kYKToastViewTextFont}
                                               context:nil];
             textSize = frame.size;
-            
         }else{
             //ios6 paul
             //        textSize = [text sizeWithFont:kYKToastViewTextFont constrainedToSize:CGSizeMake(250-30, 320) lineBreakMode:UILineBreakModeTailTruncation];
         }
-        
         if (textSize.height > 20) {
-            
             int viewHeight = 15 + textSize.height + 15;
-            
             CGRect toastFrame = toastTipView.frame;
-            
             toastFrame.size.height = viewHeight;
-            
             toastTipView.frame = toastFrame;
         }
-        
         toastTipView.tipText = text;
-        
         [toastTipView showInView:view];
     }
 }
@@ -319,16 +246,14 @@
 + (void)showToastText:(NSString *)text
                inView:(UIView *)view
       withDisplayTime:(NSTimeInterval)iSecond{
-    
     if (![NSString isEmpty:text]){
-        
-        YKToastView *toastTipView = [[YKToastView alloc] initWithFrame:CGRectMake(0, 0, 250, 44)];
-        
+        YKToastView *toastTipView = [[YKToastView alloc] initWithFrame:CGRectMake(0,
+                                                                                  0,
+                                                                                  250,
+                                                                                  44)];
         CGSize textSize = CGSizeZero;
-        
         if (SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"7.0")){
-            
-            CGRect frame = [text boundingRectWithSize:CGSizeMake(250-30, 320)
+            CGRect frame = [text boundingRectWithSize:CGSizeMake(250 - 30, 320)
                                               options:(NSStringDrawingUsesLineFragmentOrigin | NSStringDrawingUsesFontLeading)
                                            attributes:@{NSFontAttributeName:kYKToastViewTextFont}
                                               context:nil];
@@ -337,29 +262,20 @@
             //ios6 paul
             //        textSize = [text sizeWithFont:kYKToastViewTextFont constrainedToSize:CGSizeMake(250-30, 320) lineBreakMode:UILineBreakModeTailTruncation];
         }
-        
         if (textSize.height > 20) {
-            
             int viewHeight = 15 + textSize.height + 15;
-            
             CGRect toastFrame = toastTipView.frame;
-            
             toastFrame.size.height = viewHeight;
-            
             toastTipView.frame = toastFrame;
         }
-        
         toastTipView.tipText = text;
-        
         [toastTipView showInView:view withDisplayTime:iSecond];
     }
 }
 
 #pragma mark Toast样式在Window上提示自定义内容
 + (void)showToastText:(NSString *)text{
-    
     if (![NSString isEmpty:text]) {
-        
         [YKToastView showToastText:text
                             inView:[[[UIApplication sharedApplication] delegate] window]];
     }
@@ -367,9 +283,7 @@
 
 + (void)showToastText:(NSString *)text
       withDisplayTime:(NSTimeInterval)iSecond{
-    
     if (![NSString isEmpty:text]) {
-        
         [YKToastView showToastText:text
                             inView:[[[UIApplication sharedApplication] delegate] window]
                    withDisplayTime:iSecond];
@@ -377,52 +291,42 @@
 }
 
 + (void)showToastTextInRandomLocation:(NSString *)text{
-    
     if (![NSString isEmpty:text]){
-        
-        YKToastView *toastTipView = [[YKToastView alloc] initWithFrame:CGRectMake(0, 0, 250, 44)];
-        
+        YKToastView *toastTipView = [[YKToastView alloc] initWithFrame:CGRectMake(0,
+                                                                                  0,
+                                                                                  250,
+                                                                                  44)];
         CGSize textSize = CGSizeZero;
-        
         if (SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"7.0")){
-            
-            CGRect frame = [text boundingRectWithSize:CGSizeMake(250-30, 320)
+            CGRect frame = [text boundingRectWithSize:CGSizeMake(250 - 30, 320)
                                               options:(NSStringDrawingUsesLineFragmentOrigin | NSStringDrawingUsesFontLeading)
                                            attributes:@{NSFontAttributeName:kYKToastViewTextFont}
                                               context:nil];
             textSize = frame.size;
-            
         }else{
             //ios6 paul
             //      CGSize textSize = [text sizeWithFont:kYKToastViewTextFont constrainedToSize:CGSizeMake(250-30, 320) lineBreakMode:UILineBreakModeTailTruncation];
         }
-        
         if (textSize.height > 20) {
             int viewHeight = 15 + textSize.height + 15;
             CGRect toastFrame = toastTipView.frame;
             toastFrame.size.height = viewHeight;
             toastTipView.frame = toastFrame;
         }
-        
         toastTipView.tipText = text;
-        
         UIWindow *window = [[[UIApplication sharedApplication] delegate] window];
         static NSUInteger lastIndex = 0;
-        
         //Paul
         //    lastIndex = (++lastIndex) % 8;
         ++lastIndex;
-        
         lastIndex = lastIndex % 8;
-        
-        [toastTipView setCenter:CGPointMake(window.bounds.size.width/2.0, 100+lastIndex*50)];
-        
+        [toastTipView setCenter:CGPointMake(window.bounds.size.width/2.0, 100 + lastIndex * 50)];
         toastTipView.layer.opacity = 0.0;
-        
         [window addSubview:toastTipView];
-        
         [toastTipView fadeInAnimationAfterDelay:4];
     }
 }
 
+
 @end
+
